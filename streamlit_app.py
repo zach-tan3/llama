9c4d41288329a24720fb22fecf9f1c958f7fe3f7
zach-tan2 <168455222+zach-tan2@users.noreply.github.com>
2024-05-05 05:01:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,12 +20,6 @@ class Discriminator(nn.Module):
     def forward(self, input):
         return self.main(input)
 
-# Create an instance of the model
-model_path = "discriminator"  # Path to your model file in the GitHub repository
-model = Discriminator(input_size=6)  # Assuming the input size is 6, you need to update it accordingly
-model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
-model.eval()
-
 # App title
 st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 
@@ -45,17 +39,23 @@ with st.sidebar:
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
     st.subheader('Models and parameters')
-    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-70B', 'Llama2-13B', 'Llama2-7B'], key='selected_model')
+    selected_model = st.selectbox('Choose a Llama2 model', ['Llama2-70B', 'Llama2-13B', 'Llama2-7B'], key='selected_model')
     if selected_model == 'Llama2-70B':
         llm = 'replicate/llama70b-v2-chat:e951f18578850b652510200860fc4ea62b3b16fac280f83ff32282f87bbd2e48'
     elif selected_model == 'Llama2-13B':
         llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
     elif selected_model == 'Llama2-7B':
         llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
-    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=1.0, value=0.1, step=0.01)
-    top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.sidebar.slider('max_length', min_value=32, max_value=9999, value=120, step=8)
+    temperature = st.slider('temperature', min_value=0.01, max_value=1.0, value=0.1, step=0.01)
+    top_p = st.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
+    max_length = st.slider('max_length', min_value=32, max_value=9999, value=120, step=8)
     st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+
+# Create an instance of the model
+model_path = "discriminator"  # Path to your model file in the GitHub repository
+model = Discriminator(input_size=6)  # Assuming the input size is 6, you need to update it accordingly
+model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+model.eval()
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
