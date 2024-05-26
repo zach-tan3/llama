3e575106d567aa7f78901cc8f735309385b45561
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-27 02:47:42 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,15 +6,13 @@ def load_saved_patient_data():
     if os.path.exists("saved_patient_data.csv"):
         return pd.read_csv("saved_patient_data.csv")
     else:
-        return pd.DataFrame(columns=["Patient ID", "Age", "PreopEGFRMDRD", "Intraop", "ASACategoryBinned", "AnemiaCategoryBinned", "RDW15.7", "SurgicalRiskCategory", "AnesthesiaTypeCategory", "GradeofKidneyDisease", "PriorityCategory", "ICU > 24h", "Mortality"])
+        return pd.DataFrame(columns=["Patient ID", "Age", "PreopEGFRMDRD", "Intraop", "ASACategoryBinned", "AnemiaCategoryBinned", "RDW15.7", "SurgicalRiskCategory", "AnesthesiaTypeCategory", "GradeofKidneyDisease", "PriorityCategory", "ICU Probability", "Mortality Probability", "ICU Admission >24 hours", "Mortality"])
 
-def save_patient_data(patient_data):
+def save_patient_data(data):
     df = load_saved_patient_data()
-    df = df.append(patient_data, ignore_index=True)
+    df = df.append(data, ignore_index=True)
     df.to_csv("saved_patient_data.csv", index=False)
 
-def append_to_csv(filename, data):
-    df = pd.read_csv(filename)
-    df = df.append(data, ignore_index=True)
-    df.to_csv(filename, index=False)
-
+def append_to_csv(data, csv_file):
+    df = pd.DataFrame([data])
+    df.to_csv(csv_file, mode='a', index=False, header=not os.path.exists(csv_file))
