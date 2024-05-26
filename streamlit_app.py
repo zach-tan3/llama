11bfa75967a0be09ce779bd4725c0dd0e116322b
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-26 19:38:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,7 +95,7 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 # Title and description using Streamlit columns
-header_col1, header_col2, header_col3 = st.columns([1, 1, 6])
+header_col1, header_col2, header_col3 = st.columns([1, 0.5, 6])
 with header_col1:
     st.image('static/ICURISK_Logo.png', width=150)
 with header_col2:
