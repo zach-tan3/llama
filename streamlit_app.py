43e77f2cef2a2386e5156e49d2c4291e6b62fab2
zach-tan3 <yipeejrjr@gmail.com>
2024-05-26 12:27:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,12 +44,11 @@ with st.sidebar:
     st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
 
 # Create an instance of the model
-if icu_classifier==None:
-    if os.path.exists('icu_classifier.pkl') and os.path.exists('mortality_classifier.pkl'):
-        icu_classifier = joblib.load('icu_classifier.pkl')
-        mortality_classifier = joblib.load('mortality_classifier.pkl')
-    else:
-        st.error('Model files not found. Please ensure the files are uploaded.')
+if os.path.exists('icu_classifier.pkl') and os.path.exists('mortality_classifier.pkl'):
+    icu_classifier = joblib.load('icu_classifier.pkl')
+    mortality_classifier = joblib.load('mortality_classifier.pkl')
+else:
+    st.error('Model files not found. Please ensure the files are uploaded.')
         
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
