785a049235e4159fa55391e09f07c5461ed74647
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-27 02:26:59 +0800

    Update risk_model_development.py

--- risk_model_development.py
+++ risk_model_development.py
@@ -1,1 +1,18 @@
+# risk_model_development.py
+
+import streamlit as st
+
+def risk_model_development_page():
+    st.title("Risk Model Development")
+    st.write("This is the Risk Model Development page.")
+    model1_button = st.sidebar.button('Model 1')
+    model2_button = st.sidebar.button('Model 2')
+    model3_button = st.sidebar.button('Model 3')
+
+    if model1_button:
+        st.image("static/model1.png")
+    if model2_button:
+        st.image("static/model2.png")
+    if model3_button:
+        st.image("static/model3.png")
 
