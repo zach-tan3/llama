1e2fd275a87b8590edf1e144a64046fcddc8563c
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-26 17:30:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,7 +103,7 @@ st.markdown("""
 # Title and description
 st.markdown("""
 <div class='header-container'>
-    <img src='static images/ICURISK Logo.png' alt='Company Logo'>
+    <img src='/static images/ICURISK Logo.png' alt='Company Logo'>
     <div class='vertical-line'></div>
     <h1 class='main-title'>ICURISK with ChatGPT! 🤖</h1>
 </div>
