4907e6e65ec8290245e161ecae3c595205ee394e
zach-tan3 <yipeejrjr@gmail.com>
2024-05-26 14:55:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,6 @@ import os
 from dotenv import load_dotenv
 
 
-load_dotenv()
-
 openai.api_key = os.environ["OPENAI_API_KEY"]
 response = openai.chat.completions.create(
     model="gpt-3.5-turbo",
