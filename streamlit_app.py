89eb592c329885f4c788c9a50c40c4f03fcc3f3e
zach-tan3 <yipeejrjr@gmail.com>
2024-05-26 09:06:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,12 @@ with st.sidebar:
     max_length = st.slider('max_length', min_value=32, max_value=9999, value=120, step=8)
     st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
 
+if os.path.exists('icu_classifier.pkl') and os.path.exists('mortality_classifier.pkl'):
+    icu_classifier = joblib.load(icu_model_path)
+    mortality_classifier = joblib.load(mortality_model_path)
+else:
+    st.error('Model files not found. Please ensure the files are uploaded.')
+
 # Create an instance of the model
 icu_classifier = joblib.load('icu_classifier.pkl')
 mortality_classifier = joblib.load('mortality_classifier.pkl')
