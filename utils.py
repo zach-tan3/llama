fc3c0556364941ee54682aef7a1c8054647f516a
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-27 09:51:12 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
-# utils.py
-
 import pandas as pd
+import os
 
 def load_saved_patient_data():
     if os.path.exists("saved_data.csv"):
-        return pd.read_csv("saved_data.csv")
+        try:
+            return pd.read_csv("saved_data.csv")
+        except pd.errors.EmptyDataError:
+            return pd.DataFrame(columns=["Patient ID", "Age", "PreopEGFRMDRD", "Intraop", "ASACategoryBinned", "AnemiaCategoryBinned", "RDW15.7", "SurgicalRiskCategory", "AnesthesiaTypeCategory", "GradeofKidneyDisease", "PriorityCategory", "ICU Probability", "Mortality Probability", "ICU Admission >24 hours", "Mortality"])
     else:
         return pd.DataFrame(columns=["Patient ID", "Age", "PreopEGFRMDRD", "Intraop", "ASACategoryBinned", "AnemiaCategoryBinned", "RDW15.7", "SurgicalRiskCategory", "AnesthesiaTypeCategory", "GradeofKidneyDisease", "PriorityCategory", "ICU Probability", "Mortality Probability", "ICU Admission >24 hours", "Mortality"])
 
