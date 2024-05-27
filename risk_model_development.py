a0dc17035ff4617e4dea77d3f9008ececba70e4f
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-27 15:40:58 +0800

    Update risk_model_development.py

--- risk_model_development.py
+++ risk_model_development.py
@@ -3,8 +3,67 @@
 import streamlit as st
 
 def risk_model_development_page():
-    st.title("Risk Model Development 📊")
-    st.write("This is the Risk Model Development page.")
+
+# Custom CSS for styling
+    st.markdown("""
+        <style>
+        body {
+            font-family: "sans serif";
+            background-color: #f0f0f5;
+        }
+        .stButton button {
+            background-color: #6eb52f;
+            color: white;
+        }
+        .stSidebar {
+            background-color: #e0e0ef;
+        }
+        .stSidebar .stButton button {
+            background-color: #6eb52f;
+            color: white;
+        }
+        .stSidebar .stSelectbox, .stSidebar .stSlider {
+            margin-bottom: 20px;
+        }
+        .stChatMessage {
+            margin-bottom: 20px;
+        }
+        .header-container {
+            display: flex;
+            align-items: center;
+            margin-bottom: 20px;
+        }
+        .header-container img {
+            width: 200px;
+            margin-right: 20px;
+        }
+        .vertical-line {
+            border-left: 2px solid #6eb52f;
+            height: 80px;
+            margin-right: 20px;
+        }
+        .logo-text {
+            font-weight: 700;
+            font-size: 40px;
+            color: #000000;
+            padding-top: 18px;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
+    # Title and description with logo
+    LOGO_IMAGE = "static/ICURISK_Logo.png"
+    st.markdown(
+        f"""
+        <div class="header-container">
+            <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(LOGO_IMAGE, "rb").read()).decode()}">
+            <div class='vertical-line'></div>
+            <p class="logo-text">Risk Model Development 📊</p>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+    
     model1_button = st.sidebar.button('Model 1')
     model2_button = st.sidebar.button('Model 2')
     model3_button = st.sidebar.button('Model 3')
