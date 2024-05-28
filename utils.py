20841625a82a018a5860c0df683edb946f415588
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-28 21:51:16 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -47,4 +47,3 @@ def update_patient_data(patient_id, icu_status, mortality_status):
         mortality_col = sheet_instance.find("Mortality").col
         sheet_instance.update_cell(row, icu_col, icu_status)
         sheet_instance.update_cell(row, mortality_col, mortality_status)
-    return load_saved_patient_data()
