a1af9f9b86284776c04871f77cb474834a6b83bf
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-28 21:01:21 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,26 +12,27 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('dscp-saved-patient-dat
 # Authorize the clientsheet 
 client = gspread.authorize(creds)
 
-def save_patient_data(data):
-    st.sidebar.write("Saving patient data...")
-    df = load_saved_patient_data()
-    df = df.append(data, ignore_index=True)
-    df.to_csv("saved_data.csv", index=False)
-    print("Data saved successfully.")
-
-def append_to_csv(data, csv_file):
-    st.sidebar.write("Appending data...")
-    df = pd.DataFrame([data])
-    df.to_csv(csv_file, mode='a', index=False, header=not os.path.exists(csv_file))
-    print("Data appended successfully.")
-
 def load_saved_patient_data():
-    # get the instance of the Spreadsheet
+    # Get the instance of the Spreadsheet
     sheet = client.open('saved_patient_data')
-    # get the first sheet of the Spreadsheet
+    # Get the first sheet of the Spreadsheet
     sheet_instance = sheet.get_worksheet(0)
-    # get all the records of the data
+    # Get all the records of the data
     records_data = sheet_instance.get_all_records()
-    # convert the json to dataframe
+    # Convert the json to dataframe
     records_df = pd.DataFrame.from_dict(records_data)
     return records_df
+
+def save_patient_data(data):
+    df = load_saved_patient_data()
+    df = df.append(data, ignore_index=True)
+    
+    # Get the instance of the Spreadsheet
+    sheet = client.open('saved_patient_data')
+    # Get the first sheet of the Spreadsheet
+    sheet_instance = sheet.get_worksheet(0)
+    
+    # Update Google Sheets with the new data
+    for i in range(len(data)):
+        sheet_instance.append_row(df.iloc[i].tolist())
+    print("Data saved successfully.")
