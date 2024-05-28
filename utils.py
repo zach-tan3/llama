43e0caf19c224cd6c42c5cef993758c4f1a54e2f
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-28 17:56:34 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,3 +14,17 @@ def append_to_csv(data, csv_file):
     df = pd.DataFrame([data])
     df.to_csv(csv_file, mode='a', index=False, header=not os.path.exists(csv_file))
     print("Data appended successfully.")
+
+def load_saved_patient_data():
+    print("Loading saved patient data...")
+    if os.path.exists("saved_data.csv"):
+        try:
+            data = pd.read_csv("saved_data.csv")
+            print("Data loaded successfully.")
+            return data
+        except pd.errors.EmptyDataError:
+            print("Empty data error encountered. Returning empty dataframe.")
+            return pd.DataFrame(columns=["Patient ID", "Age", "PreopEGFRMDRD", "Intraop", "ASACategoryBinned", "AnemiaCategoryBinned", "RDW15.7", "SurgicalRiskCategory", "AnesthesiaTypeCategory", "GradeofKidneyDisease", "PriorityCategory", "ICU Probability", "Mortality Probability", "ICU Admission >24 hours", "Mortality"])
+    else:
+        print("File not found. Returning empty dataframe.")
+        return pd.DataFrame(columns=["Patient ID", "Age", "PreopEGFRMDRD", "Intraop", "ASACategoryBinned", "AnemiaCategoryBinned", "RDW15.7", "SurgicalRiskCategory", "AnesthesiaTypeCategory", "GradeofKidneyDisease", "PriorityCategory", "ICU Probability", "Mortality Probability", "ICU Admission >24 hours", "Mortality"])
