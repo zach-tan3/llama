1ead619da2ba51a25f200f4ac40712e71a365975
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-29 00:00:52 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -55,7 +55,7 @@ def delete_patient_data(patient_id):
     # Find the row with the given patient_id
     cell = sheet_instance.find(str(patient_id))
     if cell:
-        row = cell.row
+        cell.row = ''
         # Delete the row with the given patient_id
-        row.delete_row()
+        sheet_instance.update_cells(cell.row)
     return load_saved_patient_data()
