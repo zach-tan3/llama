aee23f2537d048cb1e7a95bbce7a540b8e9ab56f
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-29 02:52:43 +0800

    Update risk_model_development.py

--- risk_model_development.py
+++ risk_model_development.py
@@ -11,17 +11,37 @@ def risk_model_development_page():
     set_bg('static/Light blue background.jpg')
     logo3('static/ICURISK_Logo.png')
     CSS_styling()
-    
-    model1_button = st.sidebar.button('Model 1', key='model1_button')
-    model2_button = st.sidebar.button('Model 2', key='model2_button')
-    model3_button = st.sidebar.button('Model 3', key='model3_button')
-    model4_button = st.sidebar.button('Model 4', key='model4_button')
 
-    if model1_button:
-        st.image("static/ICU ROC Curve.png")
-    if model2_button:
-        st.image("static/ICU Confusion Matrix.png")
-    if model3_button:
-        st.image("static/Mortality ROC Curve.png")
-    if model4_button:
-        st.image("static/Mortality Confusion Matrix.png")
+    st.markdown("""
+        <style>
+        .stButton button {
+            background-color: #6eb52f;
+            color: white;
+            margin-bottom: 10px;
+            border-radius: 5px;
+            border: none;
+            padding: 10px;
+            font-size: 16px;
+            text-align: center;
+        }
+        .stButton button:hover {
+            background-color: #5ca024;
+        }
+        .stButton button:active {
+            background-color: #4b8520;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+    
+    roc_button = st.sidebar.button('ROC Curve Comparisons', key='roc_button')
+    cm_button = st.sidebar.button('Confusion Matrix Comparisons', key='cm_button')
+
+    if roc_button:
+        st.markdown("### ROC Curve Comparison: ICU vs. Mortality")
+        st.image("static/ICU ROC Curve.png", caption="ICU ROC Curve")
+        st.image("static/Mortality ROC Curve.png", caption="Mortality ROC Curve")
+
+    if cm_button:
+        st.image("static/ICU Confusion Matrix.png", caption="ICU Confusion Matrix")
+        st.image("static/Mortality Confusion Matrix.png", caption="Mortality Confusion Matrix")
+        st.markdown("### Confusion Matrix Comparison: ICU vs. Mortality")
