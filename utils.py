461f46426ea7f7ca55d3dbe648fbdd338aca547e
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-28 21:06:22 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ def save_patient_data(data):
     df = df.append(data, ignore_index=True)
     df.to_csv("saved_data.csv", index=False)
 
-def append_to_csv(data, csv_file):
+def append(data, csv_file):
     df = pd.DataFrame([data])
     df.to_csv(csv_file, mode='a', index=False, header=not os.path.exists(csv_file))
 
