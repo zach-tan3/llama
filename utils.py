3ae964092ff2e5fe8b351138306511557b65f909
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-28 20:51:50 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,15 +13,14 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('dscp-saved-patient-dat
 client = gspread.authorize(creds)
 
 def save_patient_data(data):
-    print("Saving patient data:", data)
-    st.sidebar.write("Saving patient data")
+    st.sidebar.write("Saving patient data...")
     df = load_saved_patient_data()
     df = df.append(data, ignore_index=True)
     df.to_csv("saved_data.csv", index=False)
     print("Data saved successfully.")
 
 def append_to_csv(data, csv_file):
-    print("Appending data to CSV:", data)
+    st.sidebar.write("Appending data...")
     df = pd.DataFrame([data])
     df.to_csv(csv_file, mode='a', index=False, header=not os.path.exists(csv_file))
     print("Data appended successfully.")
@@ -36,18 +35,3 @@ def load_saved_patient_data():
     # convert the json to dataframe
     records_df = pd.DataFrame.from_dict(records_data)
     return records_df
-    
-    '''
-    print("Loading saved patient data...")
-    if os.path.exists("saved_data.csv"):
-        try:
-            data = pd.read_csv("saved_data.csv")
-            print("Data loaded successfully.")
-            return data
-        except pd.errors.EmptyDataError:
-            print("Empty data error encountered. Returning empty dataframe.")
-            return pd.DataFrame(columns=["Patient ID", "Age", "PreopEGFRMDRD", "Intraop", "ASACategoryBinned", "AnemiaCategoryBinned", "RDW15.7", "SurgicalRiskCategory", "AnesthesiaTypeCategory", "GradeofKidneyDisease", "PriorityCategory", "ICU Probability", "Mortality Probability", "ICU Admission >24 hours", "Mortality"])
-    else:
-        print("File not found. Returning empty dataframe.")
-        return pd.DataFrame(columns=["Patient ID", "Age", "PreopEGFRMDRD", "Intraop", "ASACategoryBinned", "AnemiaCategoryBinned", "RDW15.7", "SurgicalRiskCategory", "AnesthesiaTypeCategory", "GradeofKidneyDisease", "PriorityCategory", "ICU Probability", "Mortality Probability", "ICU Admission >24 hours", "Mortality"])
-'''
