94a08d0ecfc08f529acd084ec4cc6b9339839774
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-29 02:11:36 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -85,3 +85,19 @@ def set_bg(main_bg):
          """,
          unsafe_allow_html=True
      )
+
+def logo(image):
+    # set bg name
+    main_bg_ext = "jpg"
+        
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
