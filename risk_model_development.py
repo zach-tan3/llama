8252bb731b299e9b2ab8c2dc091fa3693c640807
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-29 03:10:33 +0800

    Update risk_model_development.py

--- risk_model_development.py
+++ risk_model_development.py
@@ -13,35 +13,28 @@ def risk_model_development_page():
     logo3('static/ICURISK_Logo.png')
     CSS_styling()
 
-    with st.sidebar:
-        selected = option_menu("Main Menu", ["Home", 'Settings'], 
-            icons=['house', 'gear'], menu_icon="cast", default_index=1)
-        selected
+    selected_option = option_menu(
+        menu_title='Model Comparisons',
+        menu_icon='list-columns-reverse',
+        icons=['bar-chart-line', 'diagram-3'],
+        options=['ROC Curve Comparisons', 'Confusion Matrix Comparisons'],
+        styles={
+            "container": {"padding": "5px"},
+            "nav-link": {"font-size": "16px", "text-align": "left", "margin": "0px", "padding": "10px", "border-radius": "5px"},
+            "nav-link-selected": {"background-color": "#6eb52f", "color": "white"}
+        }
+    )
 
-    main_page_sidebar = st.sidebar.empty()
-    with main_page_sidebar:
-        selected_option = option_menu(
-            menu_title = 'Navigation',
-            menu_icon = 'list-columns-reverse',
-            icons = ['box-arrow-in-right', 'person-plus', 'x-circle','arrow-counterclockwise'],
-            options = ['Login', 'Create Account', 'Forgot Password?', 'Reset Password'],
-            styles = {
-                "container": {"padding": "5px"},
-                "nav-link": {"font-size": "14px", "text-align": "left", "margin":"0px"}} )
-    
-    roc_button = st.sidebar.button('ROC Curve Comparisons', key='roc_button')
-    cm_button = st.sidebar.button('Confusion Matrix Comparisons', key='cm_button')
-
-    if roc_button:
+    if selected_option == 'ROC Curve Comparisons':
         st.markdown("### ROC Curve Comparison: ICU vs. Mortality")
         st.markdown('<div class="image-container">', unsafe_allow_html=True)
-        st.image("static/ICU ROC Curve.png", caption="ICU ROC Curve")
-        st.image("static/Mortality ROC Curve.png", caption="Mortality ROC Curve")
+        st.image("static/ICU ROC Curve.png", caption="ICU ROC Curve", use_column_width=True)
+        st.image("static/Mortality ROC Curve.png", caption="Mortality ROC Curve", use_column_width=True)
         st.markdown('</div>', unsafe_allow_html=True)
 
-    if cm_button:
-        st.markdown('<div class="image-container">', unsafe_allow_html=True)
-        st.image("static/ICU Confusion Matrix.png", caption="ICU Confusion Matrix")
-        st.image("static/Mortality Confusion Matrix.png", caption="Mortality Confusion Matrix")
-        st.markdown('</div>', unsafe_allow_html=True)
+    if selected_option == 'Confusion Matrix Comparisons':
         st.markdown("### Confusion Matrix Comparison: ICU vs. Mortality")
+        st.markdown('<div class="image-container">', unsafe_allow_html=True)
+        st.image("static/ICU Confusion Matrix.png", caption="ICU Confusion Matrix", use_column_width=True)
+        st.image("static/Mortality Confusion Matrix.png", caption="Mortality Confusion Matrix", use_column_width=True)
+        st.markdown('</div>', unsafe_allow_html=True)
