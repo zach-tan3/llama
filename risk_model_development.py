25c8b50ea581fba666f24811233098cef27d4e5b
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-29 02:35:27 +0800

    Update risk_model_development.py

--- risk_model_development.py
+++ risk_model_development.py
@@ -10,77 +10,8 @@ def risk_model_development_page():
     # Custom CSS for styling
     set_bg('static/Light blue background.jpg')
     logo3('static/ICURISK_Logo.png')
-    st.markdown("""
-        <style>
-        body {
-            font-family: "sans serif";
-            background-color: #f0f0f5;
-        }
-        .stButton button {
-            background-color: #6eb52f;
-            color: white;
-            margin-bottom: 10px;
-            border-radius: 5px;
-            border: none;
-            padding: 10px;
-            text-align: left;
-            display: flex;
-            align-items: center;
-            font-size: 16px;
-        }
-        .stButton button:hover {
-            background-color: #5ca024;
-        }
-        .stButton button:active {
-            background-color: #4b8520;
-        }
-        .stSidebar {
-            background-color: #e0e0ef;
-        }
-        .stSidebar .stButton button {
-            background-color: transparent;
-            color: black;
-            text-align: left;
-            padding: 10px;
-            border: none;
-            border-radius: 5px;
-            font-size: 16px;
-        }
-        .stSidebar .stButton button:hover {
-            background-color: #dcdcdc;
-        }
-        .stSidebar .stButton button:active {
-            background-color: #bcbcbc;
-        }
-        .stSidebar .stSelectbox, .stSidebar .stSlider {
-            margin-bottom: 20px;
-        }
-        .stChatMessage {
-            margin-bottom: 20px;
-        }
-        .header-container {
-            display: flex;
-            align-items: center;
-            margin-bottom: 20px;
-        }
-        .header-container img {
-            width: 200px;
-            margin-right: 20px;
-        }
-        .vertical-line {
-            border-left: 2px solid #6eb52f;
-            height: 80px;
-            margin-right: 20px;
-        }
-        .logo-text {
-            font-weight: 700;
-            font-size: 40px;
-            color: #000000;
-            padding-top: 18px;
-        }
-        </style>
-        """, unsafe_allow_html=True)
-
+    CSS_styling()
+    
     model1_button = st.sidebar.button('Model 1', key='model1_button')
     model2_button = st.sidebar.button('Model 2', key='model2_button')
     model3_button = st.sidebar.button('Model 3', key='model3_button')
