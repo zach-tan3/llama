902818c3fbb7e6231a5bdac282c1bc599cc98190
WilfredL <153260549+WilfredL@users.noreply.github.com>
2024-05-29 03:54:09 +0800

    Update risk_model_development.py

--- risk_model_development.py
+++ risk_model_development.py
@@ -17,7 +17,7 @@ def risk_model_development_page():
     with st.sidebar:
         selected_option = option_menu(
             menu_title='Model Comparisons',
-            icons=['line-chart', 'diagram-3', 'bar-chart'],
+            icons=['diagram-3', 'diagram-3', 'bar-chart'],
             options=['ROC Curve Comparisons', 'Confusion Matrix Comparisons', 'Model Performance Comparisons'],
             styles={
                 "container": {"padding": "5px"},
